dd82a60bf4717fb4be8c73437a0d425594161fce
VinOfSteel <v.nascimento23@yahoo.com.br>
2024-07-26 13:50:20 -0300

    refactor: changing start script to work with only the api container in prod

--- start.sh
+++ start.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
 # Wait for the database to be ready
-echo "Waiting for database to be ready..."
 MAX_TRIES=30
 TRIES=0
 
 until pg_isready -h $PGHOST -p $PGPORT -U $PGUSER || [ $TRIES -eq $MAX_TRIES ]; do
-  echo "Waiting for database container to be up... $TRIES/$MAX_TRIES"
+  echo "Verifying if database is up... $TRIES/$MAX_TRIES"
   TRIES=$((TRIES+1))
   sleep 2
 done
 
 if [ $TRIES -eq $MAX_TRIES ]; then
-  echo "Database container is down after $MAX_TRIES attempts, exiting."
+  echo "Database is down after $MAX_TRIES attempts, exiting."
   exit 1
 fi
 
-echo "Database container is up."
+echo "Database is up."
 
-# Check if the database exists
-DB_EXISTS=$(psql -h $PGHOST -p $PGPORT -U $PGUSER -tAc "SELECT 1 FROM pg_database WHERE datname='$PGDATABASE'")
+# Check if the database exists in dev
+if [ "$ENV" != "production" ]; then
+  DB_EXISTS=$(psql -h $PGHOST -p $PGPORT -U $PGUSER -tAc "SELECT 1 FROM pg_database WHERE datname='$PGDATABASE'")
 
-if [ "$DB_EXISTS" != "1" ]; then
-  echo "Database $PGDATABASE does not exist. Creating it..."
-  createdb -h $PGHOST -p $PGPORT -U $PGUSER $PGDATABASE
-else
-  echo "Database $PGDATABASE already exists. Skipping creation."
+  if [ "$DB_EXISTS" != "1" ]; then
+    echo "Database $PGDATABASE does not exist. Creating it..."
+    createdb -h $PGHOST -p $PGPORT -U $PGUSER $PGDATABASE
+  else
+    echo "Database $PGDATABASE already exists. Skipping creation."
+  fi
 fi
 
 # Run migrations
